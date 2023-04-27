0f0fbb6d26b44b4bc68a8eb680cdbe72d82ac013
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-04-27 23:53:53 +0200

    Adds an entry

--- lib/widgets/progress_chart_utils.dart
+++ lib/widgets/progress_chart_utils.dart
@@ -2,3 +2,4 @@ import 'package:weight_tracker/model/weight_entry.dart';
 
 /// Removes entries that are before beginningDate
 /// Adds an entry at the beginning of list if there is one before and after beginningDate
+List<WeightEntry> prepareEntryList(
